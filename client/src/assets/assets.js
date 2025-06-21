4d9fb635bfde283e2a8bd7c1a8974bf5f3f15fd6
amitlaware <lawareamit279@gmail.com>
2025-06-21 19:59:39 +0530

    first commit

---
+++ client/src/assets/assets.js
@@ -0,0 +1,12 @@
+import icon from './icon.png'
+import logo from './logo.svg'
+import searchIcon from './searchIcon.svg'
+
+
+
+export const assets = {
+    icon,
+    logo,
+    searchIcon,
+    
+}
